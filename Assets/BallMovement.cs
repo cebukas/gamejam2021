2ed7e0920db3d5881417ce8496630690b9e34266
cebukas <dijokas98@gmail.com>
2021-01-29 23:43:36 +0200

    ball and ghost logic

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+     public Transform target;
+     public float speed;
+     public bool isAccelerating;
+     private Vector3 position;
+
+     public void Start(){
+        position = target.position;
+     }
+ 
+     void FixedUpdate(){
+        transform.LookAt(position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+
+        if (Vector3.Distance(transform.position, position) > 1f){ //move if distance from target is greater than 1
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0) );
+        }
+
+        if (isAccelerating)
+         speed += Time.deltaTime * 3;
+    }
+}
---
+++ Assets/Scripts/GhostMovement.cs
@@ -0,0 +1,25 @@
+﻿ using UnityEngine;
+ 
+ public class GhostMovement : MonoBehaviour {
+ 
+     public Transform target;
+     public float speed;
+     public float lifeTime;
+ 
+     void FixedUpdate(){
+        transform.LookAt(target.position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+
+        if (Vector3.Distance(transform.position, target.position) > 1f){ //move if distance from target is greater than 1
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0) );
+        }
+
+
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0){
+            Destroy(this.gameObject);
+        }
+
+    }
+ 
+}
