49539c3c2f14842eb9ceed3295c2a2243a7fab5a
cebukas <dijokas98@gmail.com>
2021-01-29 21:35:01 +0200

    main menu window
    it is possible to click on the 'Canvas' gameobject and under the 'Menu.cs' type what scene name to load

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public string sceneNameToLoad;
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public void onPlay()
+    {
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneNameToLoad);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void onBack()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
+
+    public void onSettings()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void onExit()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting (only in builds)");
+    }
+}
