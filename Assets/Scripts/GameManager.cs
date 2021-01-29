ed9b0b25c1ca613b590f831af2121cab4dc0777d
KernalPanik <lukas.michnevic@protonmail.com>
2021-01-29 22:28:33 +0200

    Saving Progress

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float TimeLeftInSeconds = 900f;
+
+    public GameObject Hero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeLeftInSeconds -= Time.deltaTime;
+        if(TimeLeftInSeconds <= 0)
+        {
+            // Start Time's up sequence
+            Debug.Log("Oy, Time's up!");
+        }
+    }
+}
