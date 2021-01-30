9b8d5f4b48a2f1f935cc5723016bb3dc34786cae
KernalPanik <lukas.michnevic@protonmail.com>
2021-01-30 14:56:42 +0200

    Death from damage now freezes the movement

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +7,17 @@ public class Health : MonoBehaviour
 {
     public int health;
     private float covidDelay;
+
+    public static event EventHandler DeathFromDamage;
+
     public void DoDamage(){
         health--;
         if(health == 0){
             Debug.Log("playing dope - die mf die ");
+            DeathFromDamage.Invoke(this, new EventArgs());
         }
     }
+
     public void Heal(){
         if(health != 3)
             health++;
