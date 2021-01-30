817a407820aaa6458dbe2c9d2952ff10f60f6950
KernalPanik <lukas.michnevic@protonmail.com>
2021-01-30 11:13:16 +0200

    Implemented basic light spread

---
+++ Assets/Scripts/SMaskFlicker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMaskFlicker : MonoBehaviour
+{
+    [Range(0.05f, 0.2f)]
+    public float FlickTime;
+
+    [Range(0.05f, 0.2f)]
+    public float AddSize;
+
+    private float _timer = 0;
+    private bool _flicked = true;
+    
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer > FlickTime)
+        {
+            if (_flicked)
+            {
+                transform.localScale = new Vector3(transform.localScale.x + AddSize, transform.localScale.y + AddSize, transform.localScale.z);
+            }
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x - AddSize, transform.localScale.y - AddSize, transform.localScale.z);
+            }
+
+            _timer = 0;
+            _flicked = !_flicked;
+        }
+    }
+}
