0a56c46428a0b46fc9053de4078a48a9b96d23d2
Daumantas Šimkus <daumantas.simkus@gmail.com>
2021-01-31 12:25:37 +0200

    Fix player movement

--- Assets/Scripts/LightRotation.cs
+++ Assets/Scripts/LightRotation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LightRotation : MonoBehaviour
 {
+    public int speedInDegrees;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class LightRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate (0,0,30*Time.deltaTime);
+        transform.Rotate (0,0,speedInDegrees*Time.deltaTime);
     }
 }
