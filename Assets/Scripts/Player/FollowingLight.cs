d8232285ea9ad3735a3d7106eb337704200e22e5
Daumantas <like.dmt@gmail.com>
2021-09-15 21:32:50 +0300

    Light follow mouse position

--- Assets/Scripts/Player/FollowingLight.cs
+++ Assets/Scripts/Player/FollowingLight.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class FollowingLight : MonoBehaviour
 {
+    private Camera mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
+        var rotZ = GetAnglesBetweenCenterAndMousePosition();
+        transform.eulerAngles = new Vector3(0f, 0f, rotZ);
     }
 
     // Update is called once per frame
-    void Update(){
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; // subtracting the position of the player from the mouse mous position 
-        //Debug.Log("Text");
-        difference.Normalize(); // normalizing the vector. meaning that all the sum of the vector will be equal to 1
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; //find the angle in degrees
-        transform.transform.rotation = Quaternion.Euler(0f, 0f, rotZ + 90 );
+    void Update()
+    {
+        var rotZ = GetAnglesBetweenCenterAndMousePosition();
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+    }
 
- }
+    // return angles needed to rotate light
+    private float GetAnglesBetweenCenterAndMousePosition()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.x -= Screen.width / 2;
+        mousePos.y -= Screen.height / 2;
+        mousePos.Normalize();
+        return Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg - 90;
+    }
 }
